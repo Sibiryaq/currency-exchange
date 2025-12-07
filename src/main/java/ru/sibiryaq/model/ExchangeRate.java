a369bbc583a2bc7a64d789a23baae3a294bd7d93
Sibiryaq <4totakoeaaa@mail.ru>
2025-12-08 01:43:32 +0700

    add model

--- src/main/java/ru/sibiryaq/model/ExchangeRate.java
+++ src/main/java/ru/sibiryaq/model/ExchangeRate.java
@@ -2,9 +2,63 @@ package ru.sibiryaq.model;
 
 import java.math.BigDecimal;
 
+/**
+ * Индексы:
+ *
+ * Первичный ключ по полю ID
+ * Unique индекс по паре полей
+ * BaseCurrencyId,
+ * TargetCurrencyId для гарантий уникальности валютной пары,
+ * и для ускорения поиска курса по паре валют
+ */
 public class ExchangeRate {
-    int id; //Айди курса обмена, автоинкремент, первичный ключ
-    Currency BaseCurrencyId; // ID базовой валюты, внешний ключ на Currencies.ID
-    Currency TargetCurrencyId; // ID целевой валюты, внешний ключ на Currencies.ID
-    BigDecimal rate; // курс обмена
+    private int id; //Айди курса обмена, автоинкремент, первичный ключ
+    private Currency BaseCurrencyId; // ID базовой валюты, внешний ключ на Currencies.ID
+    private Currency TargetCurrencyId; // ID целевой валюты, внешний ключ на Currencies.ID
+    private BigDecimal rate; // курс обмена
+    // Decimal(6) - десятичное число с 6 знаками после запятой.
+    // Полезно для валют, отличающихся на порядки
+
+
+    public ExchangeRate() {
+    }
+
+    public ExchangeRate(int id, BigDecimal rate, Currency targetCurrencyId, Currency baseCurrencyId) {
+        this.id = id;
+        this.rate = rate;
+        TargetCurrencyId = targetCurrencyId;
+        BaseCurrencyId = baseCurrencyId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Currency getBaseCurrencyId() {
+        return BaseCurrencyId;
+    }
+
+    public void setBaseCurrencyId(Currency baseCurrencyId) {
+        BaseCurrencyId = baseCurrencyId;
+    }
+
+    public Currency getTargetCurrencyId() {
+        return TargetCurrencyId;
+    }
+
+    public void setTargetCurrencyId(Currency targetCurrencyId) {
+        TargetCurrencyId = targetCurrencyId;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
+    }
 }
