3adf408b208576b908bf878506df657f18da64cb
Sibiryaq <4totakoeaaa@mail.ru>
2025-12-08 14:09:02 +0700

    add JSON Util for automatic serealization

---
+++ src/main/resources/init.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS Currencies (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Code TEXT NOT NULL UNIQUE,
+    FullName TEXT NOT NULL,
+    Sign TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ExchangeRates (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    BaseCurrencyId INTEGER NOT NULL,
+    TargetCurrencyId INTEGER NOT NULL,
+    Rate DECIMAL(10, 6) NOT NULL,
+    FOREIGN KEY (BaseCurrencyId) REFERENCES Currencies(ID),
+    FOREIGN KEY (TargetCurrencyId) REFERENCES Currencies(ID),
+    UNIQUE(BaseCurrencyId, TargetCurrencyId)
+);
